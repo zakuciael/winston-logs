45125315aabbc9fa7c5db25d057b9df47adf1bda
Krzysztof Saczuk <zakuciael@outlook.com>
2021-01-31 02:18:11 +0100

    Added custom splatter class based on the logform Splatter.

---
+++ src/lib/splat.ts
@@ -0,0 +1,64 @@
+import { Format, TransformableInfo } from "logform";
+import { formatWithOptions } from "util";
+import { SPLAT } from "triple-beam";
+
+const formatRegExp = /%[scdjifoO%]/g;
+const escapedPercent = /%%/g;
+
+interface Info extends TransformableInfo {
+    [SPLAT]?: keyof TransformableInfo;
+}
+
+interface SplatterOptions {
+    colorize?: boolean;
+}
+
+class Splatter implements Format {
+    options: SplatterOptions;
+
+    constructor(opts: SplatterOptions) {
+        this.options = opts;
+    }
+
+    public transform(info: Info): TransformableInfo | boolean {
+        const splat = info[SPLAT] || info.splat;
+        const msg = info.message;
+
+        if (!splat || !splat.length) return info;
+        const tokens = msg && msg.match && msg.match(formatRegExp);
+
+        if (!tokens && (splat || splat.length)) {
+            const metas = splat.length > 1 ? splat.splice(0) : splat;
+
+            if (metas.length)
+                for (let i = 0; i < metas.length; i++) {
+                    Object.assign(info, metas[i]);
+                }
+
+            return info;
+        }
+
+        return tokens ? this._splat(info, tokens) : info;
+    }
+
+    private _splat(info: Info, tokens: string[]): TransformableInfo {
+        const msg = info.message;
+        const splat = info[SPLAT] || info.splat || [];
+        const percents = msg.match(escapedPercent);
+        const escapes = (percents && percents.length) || 0;
+
+        const expectedSplat = tokens.length - escapes;
+        const extraSplat = expectedSplat - splat.length;
+        const metas = extraSplat < 0 ? splat.splice(extraSplat, -1 * extraSplat) : [];
+
+        if (metas.length)
+            for (let i = 0; i < metas.length; i++) {
+                Object.assign(info, metas[i]);
+            }
+
+        info.message = formatWithOptions({ colors: this.options.colorize || false }, msg, ...splat);
+        return info;
+    }
+}
+
+export const splat = (opts: SplatterOptions): Splatter => new Splatter(opts);
