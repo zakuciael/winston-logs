6cfce748333553fdc05a6958f29da4e4500c64ab
Krzysztof Saczuk <zakuciael@outlook.com>
2021-01-31 02:19:30 +0100

    Combined all required formatters into single one.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-(() => {
-    console.log("Hello, World!");
-})();
+import { messageFormatter } from "./lib/messageFormatter";
+import { prettyPrint } from "./lib/prettyPrint";
+import { splat } from "./lib/splat";
+import { format } from "logform";
+
+export const logsFormat = format.combine(
+    format.timestamp({ format: "MMM D HH:mm:ss" }),
+    format.errors({ stack: true }),
+    splat({ colorize: true }),
+    prettyPrint({ depth: 3, colorize: true }),
+    messageFormatter
+);
