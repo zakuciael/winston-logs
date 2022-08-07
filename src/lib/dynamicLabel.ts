e0c5504029721f2d5d1558c5d5b1388f1d4c605b
Krzysztof Saczuk <zakku@zakku.eu>
2022-08-07 16:31:25 +0200

    fix: resolve dynamic label from info object

--- src/lib/dynamicLabel.ts
+++ src/lib/dynamicLabel.ts
@@ -12,6 +12,9 @@ export const dynamicLabel = format((info: Info, opts: DynamicLabelOptions) => {
     // Check if the label is set in the options object.
     if (opts.label) info.label = opts.label;
 
+    // Check if the label is set in the info object.
+    if (info[LABEL]) info.label = info[LABEL];
+
     // If the info object has a SPLAT property, look for the last item in the array.
     // If it is an object, then check if it has a LABEL property.
     // If it does, then use it as the label.
